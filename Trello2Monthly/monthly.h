610d9888158e31d1b44aa4eaa6724e62caafa1e2
Phuong <phuong.tran@jvncomm.com>
2019-04-24 14:55:57 -0400

    Refactor

---
+++ Trello2Monthly/monthly.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "pch.h"
+// Current file version
+constexpr auto version = "v1.0.5";
+
+class monthly
+{
+	struct boards_info
+	{
+		std::string name;
+		std::string id;
+	};
+
+	struct list_info
+	{
+		std::string name;
+		std::string id;
+	};
+
+	struct card_info
+	{
+		std::string name;
+		std::set<std::string> labels;
+		float hour{};
+		std::string description;
+	};
+
+	std::optional<utility::string_t> trello_secrect_;
+	std::string author_;
+	std::optional<std::string> date_;
+	std::string filename_;
+
+	// Create http_client to send the request.
+	web::http::client::http_client client_;
+	web::http::client::http_client update_client_;
+
+	std::optional<std::string> check_for_update();
+	void download_update(std::optional<std::string> url);
+	void extract_files() const;
+	void call_updater() const;
+	std::vector<std::string> split_description(const std::string& input) const;
+	std::optional<std::string> get_date(const std::string& board_name) const;
+	std::string make_header() const;
+	bool check_has_custom_field(const std::string& board_id);
+	std::string get_active_boards();
+	std::vector<list_info> get_lists(const std::string& board_id);
+	std::vector<card_info> get_card(const std::string& list_id);
+	std::vector<std::string> get_labels(const std::string& board_id);
+	std::set<std::string> get_using_label(std::vector<card_info> cards);
+	bool start_console_log();
+	bool start_file_log(std::string filename);
+	std::optional<utility::string_t> parse_config();
+	std::unordered_map<std::string, std::string> create_filename_map() const;
+	void process_data();
+public:
+	
+	std::shared_ptr<spdlog::logger> console{};
+	std::shared_ptr<spdlog::logger> file{};
+	monthly();
+	void run();
+	
+};
